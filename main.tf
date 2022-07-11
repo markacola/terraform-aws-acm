d487ee15a2c1ac527e66efeacc402509a182ab7a
Mark Cola <mark@doomsdaytuna.com>
2022-07-11 23:44:00 +1000

    Update main.tf

--- main.tf
+++ main.tf
@@ -16,6 +16,7 @@ locals {
 }
 
 resource "aws_acm_certificate" "this" {
+  provider = aws.certificate
   count = local.create_certificate ? 1 : 0
 
   domain_name               = var.domain_name
@@ -43,6 +44,7 @@ resource "aws_acm_certificate" "this" {
 }
 
 resource "aws_route53_record" "validation" {
+  provider = aws.route53
   count = local.create_certificate && var.validation_method == "DNS" && var.create_route53_records && var.validate_certificate ? length(local.distinct_domain_names) : 0
 
   zone_id = var.zone_id
@@ -60,6 +62,7 @@ resource "aws_route53_record" "validation" {
 }
 
 resource "aws_acm_certificate_validation" "this" {
+  provider = aws.certificate
   count = local.create_certificate && var.validation_method != "NONE" && var.validate_certificate && var.wait_for_validation ? 1 : 0
 
   certificate_arn = aws_acm_certificate.this[0].arn
